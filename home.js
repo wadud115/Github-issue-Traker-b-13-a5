833649395dc029a2542a21b69882fcd27dc3475b
wadud115 <wadud871226@gmail.com>
2026-03-18 21:45:44 +0600

    status add and desktop and mobile search functionality fix

--- home.js
+++ home.js
@@ -1,11 +1,18 @@
 
 
+const btn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
-
+btn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 
 const BASE = "https://phi-lab-server.vercel.app/api/v1/lab";
 
 let currentTab = "all";
+
+
+
 
 
 // load issue
@@ -36,22 +43,6 @@ const loadIssues = async ()=>{
 
 
 
-// {
-//     "id": 1,
-//     "title": "Fix navigation menu on mobile devices",
-//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-//     "status": "open",
-//     "labels": [
-//         "bug",
-//         "help wanted"
-//     ],
-//     "priority": "high",
-//     "author": "john_doe",
-//     "assignee": "jane_smith",
-//     "createdAt": "2024-01-15T10:30:00Z",
-//     "updatedAt": "2024-01-15T10:30:00Z"
-// }
-
 
 const displayIssues = (issues) =>{
  const container = document.getElementById("issues");
@@ -63,12 +54,20 @@ const displayIssues = (issues) =>{
     const div = document.createElement("div");
     div.className = `card ${issue.status}`;
 
+
+     const labelsHTML = issue.labels.map(label => {
+      return `<span class="label bg-amber-300 p-1 my-3 rounded-sm   ${label.replace(" ", "-")}">${label}</span>`;
+    }).join(" ");
+
     div.innerHTML = `
      <p class="priority"> ${issue.priority}</p>
       <h4 class="font-bold text-xl ">${issue.title}</h4>
       <p class="text-[#64748B]">${issue.description}</p>
 
       
+      <div class ="">
+      <div class="labels">${labelsHTML}</div>
+      </div>
 
       
 
@@ -78,7 +77,7 @@ const displayIssues = (issues) =>{
        
 
       
-      <p class="text-[#64748B]  ">1/15/2024</p>
+      <p class="text-[#64748B]  ">${issue.createdAt}</p>
 
 
     `;
@@ -95,7 +94,9 @@ const displayIssues = (issues) =>{
 // search 
 
 async function searchIssue() {
-  const text = document.getElementById("search").value;
+  const desktop = document.getElementById("search-desktop");
+  const mobile = document.getElementById("search-mobile");
+  const text = desktop.value || mobile.value;
 
   const res = await fetch(`${BASE}/issues/search?q=${text}`);
   const data = await res.json();
@@ -120,6 +121,10 @@ function changeTab(tab, el) {
 
 
 loadIssues();
+
+
+
+
 
 
 
